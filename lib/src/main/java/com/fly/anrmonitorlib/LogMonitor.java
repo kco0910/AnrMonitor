58ad973142f08e4c298ff6b1e3caffc94d06057d
PC-20151229YDHC\Administrator <523450269@qq.com>
2018-01-31 15:59:00 +0800

    修改日志保存逻辑

--- lib/src/main/java/com/fly/anrmonitorlib/LogMonitor.java
+++ lib/src/main/java/com/fly/anrmonitorlib/LogMonitor.java
@@ -3,17 +3,23 @@ package com.fly.anrmonitorlib;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.SystemClock;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Fj on 2018/1/25.
  */
 public class LogMonitor {
     private static LogMonitor sInstance = new LogMonitor();
-    private HandlerThread mLogThread = new HandlerThread("log");
+    private HandlerThread mLogThread = new HandlerThread("log_thread");
+    private static ArrayList<String> stackList = new ArrayList<>();
     private Handler mIoHandler;
     /**1000内执行完消息则认为不卡顿*/
     private static final long TIME_BLOCK = 1000L;
+    private static final int AWAIT = 100;
+    private long preTime = 0l;
     private LogMonitor() {
         mLogThread.start();
         mIoHandler = new Handler(mLogThread.getLooper());
@@ -27,7 +33,9 @@ public class LogMonitor {
             for (StackTraceElement s : stackTrace) {
                 sb.append(s.toString() + "\n");
             }
+            stackList.add(sb.toString());
             Log.e(AnrMonitorUtil.TAG, sb.toString());
+            SystemClock.sleep(AWAIT);
         }
     };
 
@@ -37,10 +45,21 @@ public class LogMonitor {
 
 
     public void startMonitor() {
-        mIoHandler.postDelayed(mLogRunnable, TIME_BLOCK);
+        preTime = System.currentTimeMillis();
+        mIoHandler.post(mLogRunnable);
     }
 
     public void removeMonitor() {
+        if (System.currentTimeMillis() - preTime >TIME_BLOCK){
+            //保存卡顿日志到本地
+
+        }else{
+            stackList.clear();
+        }
+    }
+
+    public void close(){
+        stackList.clear();
         mIoHandler.removeCallbacks(mLogRunnable);
     }
 }
